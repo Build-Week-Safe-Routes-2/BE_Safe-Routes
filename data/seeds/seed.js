163009cb34ce6c4f9fda0dabc49df811cc115890
ebisLab <ebaiden@techie.com>
2019-09-23 13:15:52 -0400

    small changes to the table

--- data/seeds/seed.js
+++ data/seeds/seed.js
@@ -5,7 +5,7 @@ exports.seed = function (knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('users').insert([
-                { id: 1, name: 'Ace' },
+                { id: 1, name: 'Ace', },
                 { id: 2, name: 'Roman' },
                 { id: 3, name: 'Nick' },
                 { id: 4, name: 'Ian' },
