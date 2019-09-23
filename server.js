d1aac23c5ffad6cb026154736db7b2e06589145f
ebisLab <ebaiden@techie.com>
2019-09-23 09:09:09 -0400

    server file established

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 
 const express = require('express');
+
+const cors = require('cors');
+const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(session)
+const dbConfig = require('./data/dbConfig')
+
+
+const RoutesRouter = require('./routes/routes-router.js');
+
 const server = express();
+
+
+server.use(express.json());
+// server.use('/api', RoutesRouter);
+
 
 module.exports = server;
