cf329739dd3b01f1e9e72ad942bc8261a389deae
ebisLab <ebaiden@techie.com>
2019-09-23 09:29:15 -0400

    generic get request

--- routes/routes-router.js
+++ routes/routes-router.js
@@ -5,10 +5,10 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
     const name = req.query.name
-    const hash = bcrypt.hashSync(name, 14) //the highest the number the most secure
-    res.send(`The hash for ${name} is ${hash}`)
+    res.status(200).json({ message: `Welcome person!` });
+
+    // res.send(`The hash for ${name} is ${hash}`)
 
 });
 
