f7667e3f72688a9e792d0c921b2822846e2b450d
ebisLab <ebaiden@techie.com>
2019-09-23 18:49:28 -0400

    spelling

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 const secrets = require('../data/secrets.js')
 
-const environment = secerts.environment || 'development';
+const environment = secrets.environment || 'development';
 const knexConfig = require('../knexfile.js')[environment]
 
 module.exports = knex(knexConfig);
