6867a7ee3c8710d4183b9c1267e375613f17c95c
ebisLab <ebaiden@techie.com>
2019-09-23 20:07:52 -0400

    removed staging object

--- knexfile.js
+++ knexfile.js
@@ -15,22 +15,6 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
     connection: {
