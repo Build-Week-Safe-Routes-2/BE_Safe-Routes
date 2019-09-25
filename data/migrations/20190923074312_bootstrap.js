a736105e7597682c213b7a1ca24a596e0976dbc5
ebisLab <ebaiden@techie.com>
2019-09-25 14:20:23 -0400

    raw csv data created

--- data/migrations/20190923074312_bootstrap.js
+++ data/migrations/20190923074312_bootstrap.js
@@ -1,11 +1,14 @@
-
 exports.up = function (knex) {
     return knex.schema.createTable('info', data => {
         data.increments().unique();
-        data.string('witness').notNullable();
-        data.string('traffic').notNullable();
-
-
+        data.integer('LATITUDE').notNullable();
+        data.integer('LONGITUD').notNullable();
+        data.string('PROBABILITY_OF_ACCIDENT').notNullable();
+        data.integer('RISK_LEVEL').notNullable();
+        data.integer('WEATHER').notNullable();
+        data.integer('ROUTE').notNullable();
+        data.integer('TYP_INT').notNullable();
+        data.integer('TWAY_ID').notNullable();
     });
 
 };
