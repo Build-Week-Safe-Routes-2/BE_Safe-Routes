75484b2c2e8b3cf45cbdafa5cfbfa75cca04bcc3
ebisLab <ebaiden@techie.com>
2019-09-27 05:42:48 -0400

    added other requests

--- routes/routes-router.js
+++ routes/routes-router.js
@@ -14,6 +14,8 @@ router.get('/', (req, res) => {
 
 });
 
+
+
 // router.get('/data', restricted, (req, res) => {
 //     db.select('*').from('info')
 //         .then(acc => { res.status(200).json(acc) })
@@ -22,6 +24,16 @@ router.get('/', (req, res) => {
 
 router.get('/data', (req, res) => {
     db.select('*').from('info')
+        .then(acc => { res.status(200).json(acc) })
+        .catch(err => { res.json(err) })
+})
+
+router.get('/data/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    db('info').where({ id })
+        .first() //going to give me a limit of one object, the first element
         .then(acc => { res.status(200).json(acc) })
         .catch(err => { res.json(err) })
 })
@@ -38,10 +50,50 @@ router.get('/data', (req, res) => {
 //         .catch(err => { res.status(500).json(err) })
 // })
 
+router.post('/data', (req, res) => {
+    const rtData = req.body
+    // db.select('*').from('accounts')
+    db('info')
+        .insert(rtData, 'id')
+        .then(([id]) => {
+
+            db('info').where({ id })
+                .first()
+                .then(acc => { res.status(200).json(acc) })
+            // .catch(err => { res.json(err) })
+        })
+        .catch(err => {
+            console.log('error here')
+            res.json(err)
+        })
+})
+
 router.get('/data/find', (req, res) => {
     db('info')
         .then(pos => { res.status(200).json(pos) })
         .catch(err => { res.status(500).json(err) })
+})
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('info').where({ id: req.params.id })
+        .del()
+        // .first()
+        .then(count => { res.status(200).json({ message: `deleted ${count} records` }) })
+        .catch(err => { res.json(err) })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db('info').where({ id: req.params.id })
+        .update(changes)
+        // .first()
+        .then(count => { res.status(200).json({ message: `updated ${count} records` }) })
+        .catch(err => { res.json(err) })
 })
 
 
